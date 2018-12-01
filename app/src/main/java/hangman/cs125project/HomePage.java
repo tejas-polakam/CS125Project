325fc425351479be0507d3a73ec7744c8fb59b9b
tejaspolakam <polakam2@illinois.edu>
2018-12-01 17:51:22 -0600

    redoing homepage due to error

--- app/src/main/java/hangman/cs125project/HomePage.java
+++ app/src/main/java/hangman/cs125project/HomePage.java
@@ -1,8 +1,10 @@
 package hangman.cs125project;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.*;
 
 
 public class HomePage extends AppCompatActivity {
@@ -15,6 +17,31 @@ public class HomePage extends AppCompatActivity {
         setContentView(R.layout.activity_home_page);
 
         mTextView = (TextView) findViewById(R.id.text);
+        //final Button startButton = findViewById(R.id.startButton);
+        final Button instructionsButton = findViewById(R.id.Rules);
+        final Button scoreboardButton = findViewById(R.id.ScoreBoard);
+        instructionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomePage.this, InstructionsActivity.class));
+                //replace Instructions activity with actual game frame
+            }
+        });
+        scoreboardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomePage.this, ScrollingScoreBoard.class));
+            }
+        });
+        /*
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomePage.this, InstructionsActivity.class));
+                //replace Instructions activity with actual game frame
+            }
+        });
+        */
 
     }
 }
