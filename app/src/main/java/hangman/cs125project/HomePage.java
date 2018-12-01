54f4716d195804609221b9767abe275cd7cab1f7
tejaspolakam <polakam2@illinois.edu>
2018-12-01 17:35:23 -0600

    attempting to create username logs that saves input

--- app/src/main/java/hangman/cs125project/HomePage.java
+++ app/src/main/java/hangman/cs125project/HomePage.java
@@ -2,36 +2,19 @@ package hangman.cs125project;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.*;
-import android.content.*;
-import android.widget.*;
+import android.widget.TextView;
 
 
 public class HomePage extends AppCompatActivity {
+
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        final Button ruleButton = findViewById(R.id.Rules);
-        ruleButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(HomePage.this, InstructionsActivity.class));
-            }
-        });
-        final Button scoreButton = findViewById(R.id.ScoreBoard);
-        scoreButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-             public void onClick(View v) {
-                startActivity(new Intent(HomePage.this, InstructionsActivity.class));
-                //replace Instructions activity with scoreboard frame
-            }
-        });
+        mTextView = (TextView) findViewById(R.id.text);
+
     }
-
 }
