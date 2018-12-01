8b3bef57989427379a3adee1e38527bd87242378
tejaspolakam <polakam2@illinois.edu>
2018-11-30 20:37:27 -0600

    test commit push/pull

--- app/src/main/java/hangman/cs125project/HomePageNavigation.java
+++ app/src/main/java/hangman/cs125project/HomePageNavigation.java
@@ -6,6 +6,9 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Button;
+import android.view.*;
+import android.widget.*;
 
 public class HomePageNavigation extends AppCompatActivity {
 
@@ -24,21 +27,32 @@ public class HomePageNavigation extends AppCompatActivity {
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
         }
     };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        /*
+        final Button nav1 = findViewById(R.id.navigation_dashboard);
+        final Button nav2 = findViewById(R.id.navigation_home);
+        final Button nav3 = findViewById(R.id.navigation_notifications);
+        */
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page_navigation);
-
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        final Button homeButton = findViewById(R.id.button);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //code for button actions
+            }
+        });
+
+
     }
 
 }
