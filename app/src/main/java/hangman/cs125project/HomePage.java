93ee5fefbe7f06a3035e441dda7ac3550ebefd1d
tejaspolakam <polakam2@illinois.edu>
2018-12-01 14:27:25 -0600

    commit with instructions activity, homepage activity with buttons

--- app/src/main/java/hangman/cs125project/HomePage.java
+++ app/src/main/java/hangman/cs125project/HomePage.java
@@ -1,11 +1,12 @@
 package hangman.cs125project;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.content.*;
+import android.widget.*;
 
 public class HomePage extends AppCompatActivity {
 
@@ -16,12 +17,11 @@ public class HomePage extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        final Button startButton = findViewById(R.id.Start);
+        startButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                startActivity(new Intent(HomePage.this, InstructionsActivity.class));
             }
         });
     }
