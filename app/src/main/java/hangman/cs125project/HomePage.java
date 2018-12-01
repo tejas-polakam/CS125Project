f570d24c66c7ec38e192ab1d9523f7753c8a2354
tejaspolakam <polakam2@illinois.edu>
2018-12-01 16:03:40 -0600

    attempting to create username activity

--- app/src/main/java/hangman/cs125project/HomePage.java
+++ app/src/main/java/hangman/cs125project/HomePage.java
@@ -3,10 +3,10 @@ package hangman.cs125project;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.Button;
+import android.view.*;
 import android.content.*;
 import android.widget.*;
+
 
 public class HomePage extends AppCompatActivity {
 
@@ -17,11 +17,19 @@ public class HomePage extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final Button startButton = findViewById(R.id.Start);
-        startButton.setOnClickListener(new View.OnClickListener() {
+        final Button ruleButton = findViewById(R.id.Rules);
+        ruleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(HomePage.this, InstructionsActivity.class));
+            }
+        });
+        final Button scoreButton = findViewById(R.id.ScoreBoard);
+        scoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+             public void onClick(View v) {
+                startActivity(new Intent(HomePage.this, InstructionsActivity.class));
+                //replace Instructions activity with scoreboard frame
             }
         });
     }
