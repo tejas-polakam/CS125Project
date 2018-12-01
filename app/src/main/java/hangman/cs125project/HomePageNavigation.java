6ec9669c1e78338e8c395a1e7ea118fd03bef6b2
Colby <colbyyh2@illinois.edu>
2018-11-30 23:07:38 -0600

    made a scoreboard thingy

--- app/src/main/java/hangman/cs125project/HomePageNavigation.java
+++ app/src/main/java/hangman/cs125project/HomePageNavigation.java
@@ -1,11 +1,16 @@
 package hangman.cs125project;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+
 
 public class HomePageNavigation extends AppCompatActivity {
 
@@ -39,6 +44,14 @@ public class HomePageNavigation extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Button showScores = findViewById(R.id.scores_button);
+        showScores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomePageNavigation.this, ScoreBoard.class));
+            }
+        });
     }
 
 }
