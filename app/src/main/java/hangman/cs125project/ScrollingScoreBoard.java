64b5b7ac16c8cd35a6b6b0fdb7b8effa3028348a
tejaspolakam <polakam2@illinois.edu>
2018-12-01 23:02:04 -0600

    push push testing constrained values

--- app/src/main/java/hangman/cs125project/ScrollingScoreBoard.java
+++ app/src/main/java/hangman/cs125project/ScrollingScoreBoard.java
@@ -1,16 +1,25 @@
 package hangman.cs125project;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.*;
-import android.content.*;
+import android.view.View;
 import android.widget.*;
+
 public class ScrollingScoreBoard extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling_score_board);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ScrollingScoreBoard.this, HomePage.class));
+            }
+        });
+
+
     }
 }
--- app/src/main/java/hangman/cs125project/UserNameActivity.java
+++ app/src/main/java/hangman/cs125project/UserNameActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.*;
 
 public class UserNameActivity extends AppCompatActivity {
-    private String[] usernameArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
