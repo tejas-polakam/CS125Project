3f65a406aa87e8867dfb8d2d67128d1a5569e225
tejaspolakam <polakam2@illinois.edu>
2018-12-01 23:21:38 -0600

    push push testing constrained values

--- app/src/main/java/hangman/cs125project/UserNameActivity.java
+++ app/src/main/java/hangman/cs125project/UserNameActivity.java
@@ -13,6 +13,7 @@ public class UserNameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_name);
         final Button submitUsername = findViewById(R.id.submit);
+        final Button backButton = findViewById(R.id.backButtonUser);
         //final ListView
         submitUsername.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,6 +24,16 @@ public class UserNameActivity extends AppCompatActivity {
                 //replace Instructions activity with actual game frame
             }
         });
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //insert code here to store username in a list
+
+                startActivity(new Intent(UserNameActivity.this, HomePage.class));
+                //replace Instructions activity with actual game frame
+            }
+        });
+
     }
 
 
