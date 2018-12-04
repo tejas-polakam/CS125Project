54c655ac663921952eb56e7a2f0fbf24b44b3eca
tejaspolakam <polakam2@illinois.edu>
2018-12-03 19:09:06 -0600

    created List with all user inputs

--- app/src/main/java/hangman/cs125project/UserNameActivity.java
+++ app/src/main/java/hangman/cs125project/UserNameActivity.java
@@ -5,23 +5,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
+import java.util.*;
 
 public class UserNameActivity extends AppCompatActivity {
-
+    EditText nameInput;
+    String name;
+    ListView display;
+    List<String>  list;
+    int count;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_name);
+        nameInput = findViewById(R.id.userNameInput);
         final Button submitUsername = findViewById(R.id.submit);
         final Button backButton = findViewById(R.id.backButtonUser);
         //final ListView
         submitUsername.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //insert code here to store username in a list
+                count++;
+                name = nameInput.getText().toString();
+                list.add(name);
+                startActivity(new Intent(UserNameActivity.this, InstructionsActivity.class)); //replace Instructions activity with actual game frame
 
-                startActivity(new Intent(UserNameActivity.this, InstructionsActivity.class));
-                //replace Instructions activity with actual game frame
             }
         });
         backButton.setOnClickListener(new View.OnClickListener() {
